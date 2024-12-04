0a61665b5b5c6670694e3a7253750a154eed7acf
khalid-tourhzaoui <tarikkhalidoss100v3356@gmail.com>
2024-12-04 18:54:14 +0000

    feat CardHead,TabsContentText,TabsContentEmail

--- app/components/QrCodeGenerator.tsx
+++ app/components/QrCodeGenerator.tsx
@@ -1,25 +1,29 @@
 "use client";
 import React from "react";
 import { Card, CardContent} from "@/components/ui/card";
-import { Tabs,TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Link, Mail } from "lucide-react";
+import CardHead from "./CardHead";
 import TabsContentText from "./TabsContentText";
 import TabsContentEmail from "./TabsContentEmail";
-import CardTHead from "./CardTHead";
+
 function QrCodeGenerator() {
   const [url, setUrl] = React.useState("");
-  const [color, setColor] = React.useState("#ffffff");
-  const [bgColor, setBgColor] = React.useState("#057FFF");
-  const [logo, setLogo] = React.useState<string | null>(null);
-  const [logoFile, setLogoFile] = React.useState<File | null>(null);
   const [qrType, setQrType] = React.useState("link");
   const [email, setEmail] = React.useState("");
   const [subject, setSubject] = React.useState("");
   const [message, setMessage] = React.useState("");
+  const handleEmailInput = () => {
+    const mailToLink = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
+      message
+    )}`;
+
+    setUrl(mailToLink);
+  };
   return (
     <div className="relative z-30 mx-6 my-4 flex max-w-[1250px] w-full min-h-[750px] h-full">
       <Card className="flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl">
-        <CardTHead />
+        <CardHead />
         <CardContent className="flex-1">
           <div className="h-full flex flex-col md:flex-row gap-8">
             <div className="flex-1 space-y-6">
@@ -36,9 +40,9 @@ function QrCodeGenerator() {
                 </TabsList>
                 <TabsContentText url={url} setUrl={setUrl}/>
                 <TabsContentEmail email={email} setEmail={setEmail} message={message} 
-                setMessage={setMessage} subject={subject} setSubject={setSubject}/>
+                setMessage={setMessage} subject={subject} setSubject={setSubject} handleEmailInput={handleEmailInput}/>
               </Tabs>
-            </div>
+            </div> 
           </div>
         </CardContent>
       </Card>
